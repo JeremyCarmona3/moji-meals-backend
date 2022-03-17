45b0713ababf5095dd798b16abff3f3564c6baa2
Iori Takeshita <takeshitaiori@gmail.com>
2022-03-16 21:26:03 -0700

    update

--- controller/moji.js
+++ controller/moji.js
@@ -1,7 +1,6 @@
 // add stuff here
 const recipes = require('../models/moji')
 
-
 const getRecipes = (req, res) => {
   recipes.find({},(err, data) => {
     if (err) return err
@@ -9,6 +8,24 @@ const getRecipes = (req, res) => {
   })
 }
 
+const getRecipe=(req,res)=>{
+  var recipe = req.params.title
+  recipes.findOne({title:recipe},(err,data)=>{
+      if(err) return res.json(err)
+      res.json(data)
+  })
+}
+
+const getRecipebyID=(req,res)=>{
+  recipes.findById(req.params.id,(err,data)=>{
+      if(err) return err
+      res.json(data)
+  })
+}
+
+
 module.exports = {
-  getRecipes
+  getRecipes,
+  getRecipe,
+  getRecipebyID
 }
